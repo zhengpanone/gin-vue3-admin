62ced27352238c7387c95a5d81e9fb185e8bd228
zhengpanone <zhengpanone@hotmail.com>
2022-03-04 10:33:01 +0800

    feat: 数据加密工具类

---
+++ utils/encryption/encryption.go
@@ -0,0 +1,36 @@
+package encryption
+
+import (
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+// md5
+func GetMd5String(s string) string {
+	h := md5.New()
+	h.Write([]byte(s))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+// hmac
+//key随意设置 data 要加密数据
+func Hmac(key, data string) string {
+	hash := hmac.New(md5.New, []byte(key)) // 创建对应的md5哈希加密算法
+	hash.Write([]byte(data))
+	return hex.EncodeToString(hash.Sum([]byte("")))
+}
+
+func HmacSha256(key, data string) string {
+	hash := hmac.New(sha256.New, []byte(key)) //创建对应的sha256哈希加密算法
+	hash.Write([]byte(data))
+	return hex.EncodeToString(hash.Sum([]byte("")))
+}
+
+func Sha1(data string) string {
+	sha1 := sha1.New()
+	sha1.Write([]byte(data))
+	return hex.EncodeToString(sha1.Sum([]byte("")))
+}
